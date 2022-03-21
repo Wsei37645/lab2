bd1f227b6ec4907255b2bbf5e457986d34cdb2eb
Wsei37645 <37645@studentwsei.pl>
2022-03-21 14:28:47 +0100

    punkt od 1 do 4d

---
+++ src/main/java/com/example/lab2/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.lab2;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class HelloController {
+    public Button btnMain;
+    int clickCounter = 0;
+    public void onBtnAction(ActionEvent actionEvent) {
+
+        System.out.println("Kliknięcie nr " + ++clickCounter);
+    }
+}
