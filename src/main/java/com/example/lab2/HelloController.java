99f49bfc3d5dde504d65355780c02ceeec9dd56e
Wsei37645 <37645@studentwsei.pl>
2022-03-21 14:46:56 +0100

    punkt 5

--- src/main/java/com/example/lab2/HelloController.java
+++ src/main/java/com/example/lab2/HelloController.java
@@ -2,14 +2,16 @@ package com.example.lab2;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     public Button btnMain;
+    public TextField txtLicznik;
     int clickCounter = 0;
     public void onBtnAction(ActionEvent actionEvent) {
 
         System.out.println("Kliknięcie nr " + ++clickCounter);
-
+        txtLicznik.setText("Kliknięcie nr " + clickCounter);
         if(clickCounter==1)
             btnMain.setText("i jeszcze raz");
 
