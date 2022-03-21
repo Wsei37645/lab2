c80801e7b5cdc5d5ad2b6a809cad1ac90c3de0ea
Wsei37645 <37645@studentwsei.pl>
2022-03-21 17:52:29 +0100

    punkt 6 w trakcie zatrzymanym

--- src/main/java/com/example/lab2/HelloController.java
+++ src/main/java/com/example/lab2/HelloController.java
@@ -3,17 +3,16 @@ package com.example.lab2;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
 
-public class HelloController {
+public class HelloController{
     public Button btnMain;
-    public TextField txtLicznik;
-    int clickCounter = 0;
+
+    public TextField wysokosc;
+    public TextField promien ;
+    public TextField grubosc;
+    public TextField wynik;
     public void onBtnAction(ActionEvent actionEvent) {
-
-        System.out.println("Kliknięcie nr " + ++clickCounter);
-        txtLicznik.setText("Kliknięcie nr " + clickCounter);
-        if(clickCounter==1)
-            btnMain.setText("i jeszcze raz");
-
+        wynik.setText(wysokosc.getText());
     }
 }
