54231d5c0aae633bf9796bdb583606c41fbd67ac
Wsei37645 <37645@studentwsei.pl>
2022-04-01 22:21:22 +0200

    punkt 6 ukończone

--- src/main/java/com/example/lab2/HelloApplication.java
+++ src/main/java/com/example/lab2/HelloApplication.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import com.example.lab2.HelloController;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
--- src/main/java/com/example/lab2/HelloController.java
+++ src/main/java/com/example/lab2/HelloController.java
@@ -1,18 +1,33 @@
 package com.example.lab2;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputControl;
+import javafx.scene.control.*;
 
 public class HelloController{
     public Button btnMain;
-
     public TextField wysokosc;
-    public TextField promien ;
+    public TextField promien;
     public TextField grubosc;
-    public TextField wynik;
+    public Label wynik;
+
     public void onBtnAction(ActionEvent actionEvent) {
-        wynik.setText(wysokosc.getText());
+
+
+
+            double h = Double.parseDouble(wysokosc.getText());
+            double r = Double.parseDouble(promien.getText());
+            double x = Double.parseDouble(grubosc.getText());
+            double obwod = Math.PI*(2*r);
+            double pole = obwod*h;
+            double objetosc = pole*x;
+            double wagaS = objetosc * 0.00785;
+            double wagaSN = objetosc * 0.00790;
+            double cenaS = wagaS * 0.1;
+            double cenaSN = wagaSN * 0.15;
+
+
+            wynik.setText("Cena walca ze zwykłej stali wyniesie: "+String.format("%.2f", cenaS)+" zł,\noraz będzie ważyć: "+String.format("%.3f", wagaS)+" gram"+
+                        "\n\nCena walca ze stali nierdzewnej wyniesie: "+String.format("%.2f", cenaSN)+" zł,\noraz będzie ważyć: "+String.format("%.3f", wagaSN)+" gram");
+
     }
 }
